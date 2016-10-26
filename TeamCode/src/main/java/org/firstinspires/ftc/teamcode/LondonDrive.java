0a5ab09b8357e59804b58171fa47f0be50800505
LockonS483 <rumblie.gg@gmail.com>
2016-10-26 16:42:30 -0700

    LONDON SYSTEM ONLINE

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LondonDrive.java
@@ -0,0 +1,166 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Path;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Ashment on 10/13/16.
+ */
+
+/**
+ * TODO
+ * LEGEND: <Feature> [Completed][Tested][Interfaced]
+ *
+ * Motor takeover                   √--
+ * Exponential                      √--
+ * Acceleration                     √--
+ * Mechanim Movement                √--
+ * TeleOpControls                   √--
+ * Go by encoder                    ---
+ * Getters and Setters              √--
+ */
+
+public class LondonDrive extends OpMode {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////// DEFAULT SETTINGS /////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Functional Constants
+    private double exponent=3.5; //curviness of exponential curve
+    private int speedUpdatePeriod = 33; //wait between update in milliseconds
+    private double rawAcceleration = 0.1; //delta power per period
+    private double inputThreshold = 0.01; //power input threshold
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////// VARIABLES //////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Basic Stuff
+    DcMotor motFL, motFR, motBR, motBL;
+    private double speedFL=0.0,speedFR=0.0,speedBL=0.0,speedBR=0.0;
+    public double[] motorPolarity = {1,1,1,1};
+    double[] leftPolarity = {-1, 1, 1, -1}, rightPolarity = {1, -1, -1, 1}, regularPolarity = {1,1,1,1};
+
+    //Loops
+    private Timer speedUpdater,runstopper;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////// CORE METHODS ////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    public LondonDrive(DcMotor mot1, DcMotor mot2, DcMotor mot3, DcMotor mot4) {
+        motFL = mot1;
+        motFR = mot2;
+        motBL = mot3;
+        motBR = mot4;
+
+        LondonSetup();
+
+        telemetry.addData("LondonDrive: ", "Activated.");
+    }
+
+    public void LondonSetup(){
+        //speedUpdater = new Timer("speedUpdater",true);
+        //speedUpdater.scheduleAtFixedRate(new speedUpdateTask(), 0, speedUpdatePeriod);
+        motFL.setDirection(DcMotor.Direction.REVERSE);
+        motBL.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("LondonDrive: ", "Setup.");
+    }
+
+    public void SetPower(double L, double R, boolean std){
+        if(Math.abs(L)<inputThreshold)L=0.0;
+        if(Math.abs(R)<inputThreshold)R=0.0;
+        if(std){
+            motorPolarity = regularPolarity;
+        }
+        speedFL=L;
+        speedFR=R;
+        speedBL=L;
+        speedBR=R;
+        UpdateMotorPower();
+    }
+
+    public void SetStrafe(double input){
+        if(Math.abs(input) > inputThreshold){
+            if(input < 0){
+                motorPolarity = leftPolarity;
+            }else{
+                motorPolarity = rightPolarity;
+            }
+
+            SetPower(input, input, false);
+        }
+    }
+
+    private void UpdateMotorPower() {
+        try {
+            motFL.setPower(speedFL * motorPolarity[0]);
+            telemetry.addData("motFL", speedFL);
+            motFR.setPower(speedFR * motorPolarity[1]);
+            telemetry.addData("motFR", speedFR);
+            motBL.setPower(speedBL * motorPolarity[2]);
+            telemetry.addData("motBL", speedBL);
+            motBR.setPower(speedBR * motorPolarity[3]);
+            telemetry.addData("motBR", speedBR);
+            telemetry.addData("Confirmed: ", "MOTORS OPERATIONAL");
+        } catch (Exception e) {
+            telemetry.addData("ERROR: ", e.toString());
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////// GETTERS AND SETTERS /////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public DcMotor getMotFL() {
+        return motFL;
+    }
+    public void setMotFL(DcMotor motFL) {
+        this.motFL = motFL;
+    }
+    public DcMotor getMotFR() {
+        return motFR;
+    }
+    public void setMotFR(DcMotor motFR) {
+        this.motFR = motFR;
+    }
+    public DcMotor getMotBL() {
+        return motBL;
+    }
+    public void setMotRL(DcMotor motRL) {
+        this.motBL = motRL;
+    }
+    public DcMotor getMotBR() {
+        return motBR;
+    }
+    public void setMotRR(DcMotor motRR) {
+        this.motBR = motRR;
+    }
+    public double getInputThreshold() {
+        return this.inputThreshold;
+    }
+    public void setInputThreshold(double in) {
+        this.inputThreshold = in;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LondonOp.java
@@ -0,0 +1,139 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.TimerTask;
+
+/**
+ * Created by Ashment on 10/17/16.
+ */
+
+@TeleOp (name="LondonOp", group="Test")
+public class LondonOp extends OpMode{
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////// VARIABLES ////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Basics
+    DcMotor motfl,motfr,motrr,motrl;
+    PID pidfl, pidfr, pidrr, pidrl;
+
+    //Drivers
+    LondonDrive london;
+    ButtonState joy1, joy2;
+
+    @Override
+    public void init(){
+        //Hardware Setup
+        setupMotors();
+
+        //Drivers and Objects Setup
+        try{
+            joy1 = new ButtonState();
+            joy2 = new ButtonState();
+        }catch (Exception e){
+            telemetry.addData("ERROR:", e.toString());
+        }
+
+        try{
+            london = new LondonDrive(motfl, motfr, motrl, motrr);
+            telemetry.addData("Sucess: ", "LondonDrive Setup Complete.");
+        }catch (Exception e){
+            telemetry.addData("ERROR: ", "LondonDrive Setup Failure.");
+        }
+    }
+
+    @Override
+    public void loop(){
+        //Input Detection
+        joy1.update(gamepad1);
+        joy2.update(gamepad2);
+
+        /////////////Loop Methods/////////////
+        //update movements
+        UpdateMovementInput();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// CORE METHODS //////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void UpdateMovementInput(){
+        float lSticky, rSticky;
+        lSticky = gamepad1.left_stick_y;
+        rSticky = gamepad1.right_stick_y;
+
+        telemetry.addData("LStickY: ", lSticky);
+        telemetry.addData("RStickY: ", rSticky);
+
+        if(lSticky < london.getInputThreshold() && rSticky < london.getInputThreshold()){
+            telemetry.addData("LTrigger: ", joy1.left_trigger);
+            telemetry.addData("RTrigger: ", joy1.right_trigger);
+            if(joy1.left_trigger > london.getInputThreshold()){
+                london.SetStrafe(-1 * joy1.left_trigger);
+                telemetry.addData("STRAFE L: ", (-1 * joy1.left_trigger));
+            }else if(joy1.right_trigger > london.getInputThreshold()){
+                london.SetStrafe(1 * joy1.right_trigger);
+                telemetry.addData("STRAFE R: ", (1 * joy1.right_trigger));
+            }
+        }else{
+            //Analog Movement Input
+            london.SetPower(lSticky, rSticky, true);
+            telemetry.addData("FORWARD! ", "L: " + lSticky + ", R: " + rSticky);
+        }
+
+
+        //Strafe Input (Trigger analog input between 0 and 1)
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// SETUP METHODS /////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void setupMotors(){
+        try {
+            motfr=hardwareMap.dcMotor.get("fr");
+            telemetry.addData("Confirmed: ", "Motor FR");
+        }catch (Exception e){
+            telemetry.addData("ERROR",e.toString());
+        }
+        try {
+            motfl=hardwareMap.dcMotor.get("fl");
+            telemetry.addData("Confirmed: ", "Motor FL");
+        }catch (Exception e){
+            telemetry.addData("ERROR",e.toString());
+        }
+        try {
+            motrr=hardwareMap.dcMotor.get("rr");
+            telemetry.addData("Confirmed: ", "Motor RR");
+        }catch (Exception e){
+            telemetry.addData("ERROR",e.toString());
+        }
+        try {
+            motrl=hardwareMap.dcMotor.get("rl");
+            telemetry.addData("Confirmed: ", "Motor RL");
+        }catch (Exception e){
+            telemetry.addData("ERROR",e.toString());
+        }
+    }
+
+    public void AddTelemetry(String xx, String yy){
+        telemetry.addData(xx, yy);
+    }
+
+
+    /*
+    private class speedUpdateTask extends TimerTask {
+        @Override
+        public void run() {
+
+        }
+    }*/
+
+}
