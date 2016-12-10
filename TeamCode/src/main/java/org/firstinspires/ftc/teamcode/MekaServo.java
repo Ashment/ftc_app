d0425632b0c36f59bd7b20cbff6347849eaa293f
StoryScene <Emma@Emmas-MacBook-Pro-2.local>
2016-12-09 17:42:32 -0800

    Revert "Small changes"
    This reverts commit 1213f7c3da1add24ce51b34bfe217c71ae4c9797.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MekaServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MekaServo.java
@@ -19,12 +19,8 @@ public class MekaServo extends OpMode {
     CRServo button, gate;
     protected boolean gateGo;
 
-    private double buttonPwr = 0.5;
-    private double gatePwr = 0.15;
-
-    //In milliseconds
-    private long gateTime = 300;
-    private long buttonTime = 1000;
+    protected double buttonPwr = 0.5;
+    protected double gatePwr = 0.15;
 
     //Constructor
     public MekaServo(CRServo butt, CRServo gatt){
@@ -56,7 +52,7 @@ public class MekaServo extends OpMode {
             public void run() {
                 button.setPower(0);
             }
-        }, buttonTime);
+        }, 1000);
     }
 
     public void turnGate() {
@@ -66,7 +62,7 @@ public class MekaServo extends OpMode {
             public void run() {
                 gate.setPower(0);
             }
-        }, gateTime);
+        }, 300);
     }
 
     //Toggle between spinning and not spinning
